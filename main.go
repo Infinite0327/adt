355760eb23f35c75cebf2a527442924ed846a333
Infinite0327 <1969629453@qq.com>
2026-03-28 22:00:42 +0800

    完成了基本任务和进阶任务的functor和monad

--- main.go
+++ main.go
@@ -29,6 +29,16 @@ func Cons[A any](x A, xs List[A]) List[A] {
     }
 }
 
+func MatchList[A any, R any](l List[A], onNil func() R, onCons func(head A, tail List[A]) R) R {
+	switch l.tag {
+	case tagNil:
+		return onNil()
+	case tagCons:
+		return onCons(l.head, *l.tail)
+	default:
+		panic("Unknown List state")
+	}
+}
 //实现maybe
 type maybeTag int
 const (
@@ -49,15 +59,15 @@ func Just[A any](val A) Maybe[A] {
     return Maybe[A]{tag: tagJust, value: val}
 }
 
-func (m Maybe[A]) Match(onNone func(), onJust func(val A)) {
-    switch m.tag {
-    case tagNone:
-        onNone() 
-    case tagJust:
-        onJust(m.value)
+func MatchMaybe[A any, R any](m Maybe[A], onNone func() R, onJust func(A) R) R {
+	switch m.tag {
+	case tagNone:
+		return onNone()
+	case tagJust:
+		return onJust(m.value)
 	default:
-        panic("Unknown Maybe state!")
-    }
+		panic("Unknown Maybe state")
+	}
 }
 
 // 实现either
@@ -81,27 +91,23 @@ func Right[A any, B any](b B) Either[A, B] {
     return Either[A, B]{tag: tagRight, right: b}
 }
 
-func (e Either[A,B]) Match (onLeft func(A),onRight func(B)){
-	switch e.tag{
+func MatchEither[A any, B any, R any](e Either[A, B], onLeft func(A) R, onRight func(B) R) R {
+	switch e.tag {
 	case tagLeft:
-		onLeft(e.left)
+		return onLeft(e.left)
 	case tagRight:
-		onRight(e.right)
+		return onRight(e.right)
 	default:
-        panic("Unknown Either state!")
+		panic("Unknown Either state")
 	}
 }
 
-//实现head函数
+//实现head函数 对于nil list返回的是0 
 func Head[A any](l List[A]) Maybe[A] {
-    switch l.tag {
-    case tagNil:
-        return None[A]() 
-    case tagCons:
-        return Just(l.head)
-    default:
-        panic("Unknown List state!")
-    }
+	return MatchList(l,
+		func() Maybe[A] { return None[A]() },
+		func(h A, t List[A]) Maybe[A] { return Just(h) },
+	)
 }
 
 //除0 的一个either处理错误的例子 
@@ -121,3 +127,39 @@ func Lookup[K comparable, V any](m map[K]V, key K) Maybe[V] {
     return Just(val)
 }
 
+//Maybe 的 fmap
+func MapMaybe[A any, B any](f func(A) B, ma Maybe[A]) Maybe[B] {
+	return MatchMaybe(ma,
+		func() Maybe[B] { return None[B]() },
+		func(val A) Maybe[B] { return Just(f(val)) },
+	)
+}
+
+//List 的 fmap 它返回的是一个List的类型 要么是Cons要么是None
+func MapList[A any, B any](f func(A) B, la List[A]) List[B] {
+	onNil := func() List[B] { 
+        return Nil[B]() 
+    }
+	onCons := func(h A, t List[A]) List[B] {
+        newHead := f(h)           
+        newTail := MapList(f, t)  
+        return Cons(newHead, newTail) 
+    }
+	return MatchList(la, onNil, onCons)
+}
+
+//maybe 的 Monad
+func ReturnMaybe[A any](a A) Maybe[A] {
+    return Just(a)
+}
+
+func BindMaybe[A any, B any](m Maybe[A], f func(A) Maybe[B]) Maybe[B] {
+    return MatchMaybe(m,
+        func() Maybe[B] { 
+            return None[B]() 
+        },
+        func(a A) Maybe[B] {
+            return f(a) 
+        },
+    )
+}
